155ca6436317d6dbce14da4e0ee91abced0044bc
Roopa-S-Biradar <roopa@magnovaventures.com>
2026-02-19 09:45:44 +0530

    making ajax calls

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,8 +5,39 @@ const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
+// https://countries-api-836d.onrender.com/countries/
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const getCountryInfo = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        console.log(data.name.common);
+        const html = `<article class="country">
+          <img class="country__img" src="${data?.flags?.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data?.name?.common}</h3>
+            <h4 class="country__region">${data?.region}</h4>
+            <p class="country__row"><span>👫</span>${(data?.population / 1000000).toFixed(1)
+            }</p>
+            <p class="country__row"><span>🗣️</span>${data?.languages?.por}</p>
+            <p class="country__row"><span>💰</span>${data?.currencies?.EUR?.name}</p>
+          </div>
+        </article>`;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+}
+getCountryInfo('portugal');
+getCountryInfo('india');
+getCountryInfo('germany');
+getCountryInfo('usa');
+
+
+
